3515ee6a47a88686b82caedd831ef3f5714c66f2
Ilshat Manakhov <ilshat.manakhov@waveaccess.ru>
2020-08-18 20:44:08 +0300

    * footer navbar

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -18,6 +18,14 @@ const Footer = () => (
       <img src="assets/svg/whatsapp-sml.svg" />
     </div>
   </div>
+  <div className={style.footer_navbar}>
+    <a href="/contact">IT-услуги</a>
+    <a href="/contact">Услуги для стартапов</a>
+    <a href="/contact">IT-компаниям</a>
+    <a href="/contact">О нас</a>
+    <a href="/contact">Контакты</a>
+    <a href="/contact">Вакансии</a>    
+  </div>
 </div>
 )
 
